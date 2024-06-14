daaed41b1ba48518c6a612b3bbb32b1b44f9e131
Scott Johnstone <scottajohnstone@gmail.com>
2024-06-14 09:29:33 -0500

    added ability to make todo items as completed without deleting them

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+function completeTodo(event) {
+    const value = event.target.getAttribute('aria-checked');
+    if(value !== 'true'){
+        event.target.setAttribute('aria-checked', true);
+    } else {
+        event.target.setAttribute('aria-checked', false);
+    }
+}
 
 function removeTodo(event){
-    event.parentNode.remove();
+    event.target.parentNode.remove();
 }
 
 function addTodo(event) {
     event.preventDefault();
     const item = document.createElement('li');
     item.innerText = document.querySelector('#item').value;
+    item.addEventListener('click', completeTodo);
 
     const button = document.createElement('button');
     button.innerText = 'X';
