0a33364a58751f605a1e177d8761f67fe94f5c39
Scott Johnstone <scottajohnstone@gmail.com>
2024-06-14 08:48:59 -0500

    added functions for addTodo and removeTodo

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function removeTodo(event){
+    event.parentNode.remove();
+}
+
+function addTodo(event) {
+    event.preventDefault();
+    const item = document.createElement('li');
+    item.innerText = document.querySelector('#item').value;
+
+    const button = document.createElement('button');
+    button.innerText = 'X';
+    button.addEventListener('click', removeTodo);
+    item.appendChild(button)
+
+    const list = document.querySelector('ul');
+    list.append(item);
+}
+
+document.querySelector('form').addEventListener('submit', addTodo);
